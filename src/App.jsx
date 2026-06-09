5fc94c2a4411fda68fb62ed4e195b2e0c1ff81ef
Estivendelgado16 <brallamdelgado351@gmail.com>
2026-06-09 14:39:05 -0500

    Update navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import './App.css' // Por si tienes estilos globales aquí
+import NavBar from "../components/NavBar";
+import HeroVideo from "./components/HeroVideo";
+import Gallery from "./components/Gallery";
+import InfoCard from "./components/InfoCard";
 
-function App() {
+import { stories } from "../data/stories";
+
+function Home() {
   return (
-    <div className="app-container">
-      {/* Si en el futuro quieres un Footer o Navbar global para TODAS las páginas, iría aquí */}
-      <Outlet /> 
-    </div>
-  )
+    <>
+      <NavBar />
+
+      <main className="home">
+
+        <HeroVideo />
+
+        <section>
+
+          <h2>Galería de Éxitos</h2>
+
+          <div className="content-layout">
+
+            <Gallery stories={stories} />
+
+            <aside className="sidebar">
+
+              <InfoCard
+                title="¿Sabías que...?"
+                description="Cada adopción salva dos vidas."
+                buttonText="Ver más"
+              />
+
+              <InfoCard
+                title="★★★★★"
+                description="95% de adopciones exitosas."
+              />
+
+              <InfoCard
+                title="Únete al movimiento"
+                description="Ayuda a más animales."
+                buttonText="Donar"
+              />
+
+            </aside>
+
+          </div>
+
+        </section>
+
+      </main>
+    </>
+  );
 }
 
-export default App;
+export default Home;
